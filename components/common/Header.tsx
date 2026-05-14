16db4075525efc2a716eefd7c617f255b249b5b7
kandalog <asuasu.tee@gmail.com>
2026-05-15 00:23:09 +0900

    feat: Headerのスタイリング

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,43 +1,69 @@
 import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+const navItemInfo = [
+  {
+    text: "個人開発",
+    link: "#",
+  },
+  {
+    text: "記事",
+    link: "#",
+  },
+  {
+    text: "お問い合わせ",
+    link: "#",
+  },
+];
 
 export const Header = () => {
   return (
     <>
-      {/* ヘッダー */}
-      <header className="header">
-        <div className="header__inner">
-          {/* ヘッダーの左側 */}
-          <div className="header__title">
-            <Image
-              src="/header-img.svg"
-              width={40}
-              height={40}
-              alt="ヘッダーのアイコン"
-            ></Image>
-            <p className="header__title">kandalog</p>
-          </div>
-          {/* ヘッダーの右側 */}
-          <nav className="header__nav">
-            <ul className="header__list">
-              <li className="header__item">
-                <a href="" className="header__item__link">
-                  個人開発
-                </a>
-              </li>
-              <li className="header__item">
-                <a href="" className="header__item__link">
-                  記事
-                </a>
-              </li>
-              <li className="header__item">
-                <a href="" className="header__item__link">
-                  お問い合わせ
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <header className="h-12 flex shadow rounded-md px-3">
+        <Logo />
+        <NavList navItem={navItemInfo} />
       </header>
     </>
+  );
+};
+
+const Logo = () => {
+  return (
+    <div className="flex items-center cursor-pointer">
+      <Image
+        src="/header-img.svg"
+        width={30}
+        height={40}
+        alt="ヘッダーのアイコン"
+      ></Image>
+      <Link href="/" className="font-bold text-lg">
+        kandalog
+      </Link>
+    </div>
+  );
+};
+
+const NavList = ({ navItem }: { navItem: typeof navItemInfo }) => {
+  return (
+    <nav className="ml-auto">
+      <ul className="flex items-stretch space-x-6 h-full">
+        {navItem.map((item) => (
+          <NavItem key={item.text} link={item.link}>
+            {item.text}
+          </NavItem>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+const NavItem = ({ children, link }: { children: ReactNode; link: string }) => {
+  return (
+    <li>
+      <a href={link} className="flex items-center h-full text-xs">
+        {children}
+      </a>
+    </li>
   );
 };
